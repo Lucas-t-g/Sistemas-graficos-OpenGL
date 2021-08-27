875cf9218d2fbf68002d45ee35b7196f481991e1
lucas-t-g <lucastarsitanogarcia@gmail.com>
2021-08-26 23:54:05 -0300

    implementado a rotaçao do hexagono em torno do proprio eixo

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <GL/glut.h>
 //#endif
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
@@ -115,13 +116,17 @@ void linha(float x1, float y1, float z1, float x2, float y2, float z2, float esp
 }
 
 void hexagono3d(){
-   RotateY(10);
+   float z1 = -5, z2 = -7;
+   const int sides2 = 6;  // The amount of segment to create the circle
+   const double radius2 = 3.5; // The radius of the circle
+
+   // Translate(0, 0, +(z1 + (z2-z1)/2) );
+   // RotateY(0);
+   // Translate(0, 0, -(z1 + (z2-z1)/2) );
+   // printf("z: %f", -(z1 + (z2-z1)/2));
    RotateZ(30);
    glColor3f(0.0, 0.0, 1.0);
 
-   float z1 = -3, z2 = -5;
-   const int sides2 = 6;  // The amount of segment to create the circle
-   const double radius2 = 3.5; // The radius of the circle
    glBegin(GL_QUAD_STRIP); // faces laterais
    for (int a = 0; a <= 360; a += 360 / sides2)
    {
@@ -131,27 +136,27 @@ void hexagono3d(){
    }
    glEnd();
 
-   // // glPushMatrix();
-   // glColor3f(0.0, 0.0, 0.5);
-   // // glPopMatrix();
-   // glBegin(GL_POLYGON); // face da frente
-   // for (int a = 0; a <= 360; a += 360 / sides2)
-   // {
-   //    double heading = a * 3.1415926535897932384626433832795 / 180;
-   //    glVertex3f(cos(heading) * radius2, sin(heading) * radius2, z1);
-   // }
-   // glEnd();
+   // glPushMatrix();
+   glColor3f(0.0, 0.0, 0.5);
+   // glPopMatrix();
+   glBegin(GL_POLYGON); // face da frente
+   for (int a = 0; a <= 360; a += 360 / sides2)
+   {
+      double heading = a * 3.1415926535897932384626433832795 / 180;
+      glVertex3f(cos(heading) * radius2, sin(heading) * radius2, z1);
+   }
+   glEnd();
 
-   // // glPushMatrix();
-   // glColor3f(0.5, 0.0, 0.5);
-   // // glPopMatrix();
-   // glBegin(GL_POLYGON); // face da trás
-   // for (int a = 0; a <= 360; a += 360 / sides2)
-   // {
-   //    double heading = a * 3.1415926535897932384626433832795 / 180;
-   //    glVertex3f(cos(heading) * radius2, sin(heading) * radius2, z2);
-   // }
-   // glEnd();
+   // glPushMatrix();
+   glColor3f(0.5, 0.0, 0.5);
+   // glPopMatrix();
+   glBegin(GL_POLYGON); // face da trás
+   for (int a = 0; a <= 360; a += 360 / sides2)
+   {
+      double heading = a * 3.1415926535897932384626433832795 / 180;
+      glVertex3f(cos(heading) * radius2, sin(heading) * radius2, z2);
+   }
+   glEnd();
 }
 
 void display(void){
@@ -161,12 +166,16 @@ void display(void){
    glEnable(GL_DEPTH_TEST);
    glDepthFunc(GL_LEQUAL);
    glClearDepth(1000.0f);
+
+   // RotateX(10);
+   // RotateY(90);
+   float z_coord_lines = -3;
    // eixo x
    glColor3f(1, 0, 0);
-   linha(-40, 0, 0, 40, 0, 0, 2);
+   linha(-40, 0, z_coord_lines, 40, 0, z_coord_lines, 2);
    // eixo y
    glColor3f(0, 1, 0);
-   linha(0, -40, 0, 0, 40, 0, 2);
+   linha(0, -40, z_coord_lines, 0, 40, z_coord_lines, 2);
    // eixo z
    glColor3f(0, 0, 1);
    linha(0, 0, -40, 0, 0, 40, 2);
@@ -183,16 +192,16 @@ void reshape(int w, int h) {
    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
    glMatrixMode (GL_PROJECTION);
    // glFrustum(-10.0, 10.0, -10.0, 10.0, 1.5, 20.0);
-   // Frustum(-20.0, 20.0, -20.0, 20.0, 1.5, 40.0);
+   Frustum(-10.0, 10.0, -10.0, 10.0, 1.5, 20.0);
 
-   // glOrtho(-10.0, 10.0, -10.0, 10.0, -1.5, 200.0);
-   Ortho(-10.0, 10.0, -10.0, 10.0, -5, 200.0);
+   // glOrtho(-10.0, 10.0, -10.0, 10.0, -10, 200.0);
+   // Ortho(-10.0, 10.0, -10.0, 10.0, -10, 200.0);
    glMatrixMode(GL_MODELVIEW);
 }
 
 int main(int argc, char** argv){
    glutInit(&argc, argv);
-   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
    glutInitWindowSize(1000, 1000);
    glutInitWindowPosition(10, 10);
    glutCreateWindow(argv[0]);
